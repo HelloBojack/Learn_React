df2915ecefedaf32e6da8ee7f357907b1ecae115
Bojack <695668678@qq.com>
2020-02-23 11:05:11 +0800

    webpack base

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// 创建插件实例
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: path.join(__dirname,'./src/index.html'), //源文件
+  filename: 'index.html' //生成到内存中的首页名称
+})
+
 // 向外暴露打包配置对象
 // webpack基于node构建，支持所有node语法
 module.exports={
   mode:'development',
   // mode: 'production'
+  plugins:[
+    htmlPlugin
+  ]
 }
