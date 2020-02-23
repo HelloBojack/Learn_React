211c19c179e6e26a1f374cd6e6bbcbe246f5453b
Bojack <695668678@qq.com>
2020-02-24 00:14:27 +0800

    react base

--- src/index.js
+++ src/index.js
@@ -1,2 +1,43 @@
-console.log('ok');
-console.log("ok");
+// 1、导入react和react-dom
+import React from "react";
+import ReactDom from "react-dom";
+
+// 2、创建虚拟DOM元素
+// 参数1 字符串 元素类型
+// 参数2 对象或null 这个DOM元素的属性
+// 参数3 子节点 （DOM或者文本）
+// 参数n 其他子节点
+// const myH1 = React.createElement('h1', {id :'h1',title:'h1_title'}, '这是一个h1')
+
+// JSX本质还是内部转化为React.createElement
+let myH1 = (
+  <h2 id="h2" title="h2_title">
+    这是h1
+  </h2>
+);
+let myArr1 = [<h1> h1 </h1>, <h2> h2 </h2>];
+let myArr2 = ["徐乐康","董志雯"]
+let a = 10;
+let h2Title = "h2_title";
+
+function Hello(name) {
+  return <h1>Hello {name}</h1> 
+}
+
+
+// clase -> className
+// for -> htmlFor
+let myDiv = (
+  <div id="h2" title={h2Title}>
+    {a + "1"}
+    {myH1}
+    {myArr1}
+    {myArr2.map(n=><h1 key={n}>{n}</h1>)}
+    <Hello></Hello>
+  </div>
+);
+
+// 3、react-dom
+// 参数1 渲染的虚拟DOM
+// 参数2 指定容器
+ReactDom.render(myDiv, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,25 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 // 创建插件实例
 const htmlPlugin = new HtmlWebpackPlugin({
-  template: path.join(__dirname,'./src/index.html'), //源文件
+  template: path.join(__dirname, './src/index.html'), //源文件
   filename: 'index.html' //生成到内存中的首页名称
 })
 
 // 向外暴露打包配置对象
 // webpack基于node构建，支持所有node语法
-module.exports={
-  mode:'development',
+// webpack默认只打包.js，需要配置第三方loader
+module.exports = {
+  mode: 'development',
   // mode: 'production'
-  plugins:[
+  plugins: [
     htmlPlugin
-  ]
+  ],
+  // 所有第三方模块
+  module: {
+    rules: [{
+      test: /\.js|jsx$/,
+      use: 'babel-loader',
+      exclude: /node_modules/
+    }]
+  }
 }
